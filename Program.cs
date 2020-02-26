2bf154d09b14984fd0babe741763136b708e7b2f
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2020-02-26 10:45:27 -0600

    done

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace charp_lists_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+};
+
+            Console.WriteLine(@"
+                         _______
+         ..-'`       ````---.
+       .'          ___ .'````.'SS'.
+      /        ..-SS####'.  /SSHH##'.
+     |       .'SSSHHHH##|/#/#HH#H####'.
+    /      .'SSHHHHH####/||#/: \SHH#####\
+   /      /SSHHHHH#####/!||;`___|SSHH###\
+-..__    /SSSHHH######.         \SSSHH###\
+`.'-.''--._SHHH#####.'           '.SH####/
+  '. ``'-  '/SH####`/_             `|H##/
+  | '.     /SSHH###|`'==.       .=='/\H|
+  |   `'-.|SHHHH##/\__\/        /\//|~|/
+  |    |S#|/HHH##/             |``  |
+  |    \H' |H#.'`              \    |
+  |        ''`|               -     /
+  |          /H\          .----    /
+  |         |H#/'.           `    /
+  |          \| | '..            /
+  |    ^~DLF   /|    ''..______.'
+   \          //\__    _..-. | 
+    \         ||   ````     \ |_
+     \    _.-|               \| |_
+     _\_.-'   `'''''-.        |   `--.
+ ''``    \            `''-;    \ /
+          \      .-'|     ````.' -
+          |    .'  `--'''''-.. |/
+          |  .'               \|
+          |.'
+            ");
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list.
+                // Print a message to the console indicating whether the index is in the list.
+                if (numbers.Contains(i))
+                {
+                    Console.WriteLine($"index number {i} is one of the rando numeros");
+                }
+            }
+        }
+    }
+}
